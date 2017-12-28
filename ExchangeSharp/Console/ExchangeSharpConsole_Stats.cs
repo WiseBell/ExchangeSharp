aed0b9d269000922e975fa7e537090435ef3984c
노현종 <neoswirl@gmail.com>
2017-12-28 14:22:04 +0900

    콘솔 테스트 코드 수정

--- ExchangeSharp/Console/ExchangeSharpConsole_Stats.cs
+++ ExchangeSharp/Console/ExchangeSharpConsole_Stats.cs
@@ -15,6 +15,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Text;
 using System.Threading;
 
 namespace ExchangeSharp
@@ -23,48 +24,43 @@ namespace ExchangeSharp
     {
         public static void RunShowExchangeStats(Dictionary<string, string> dict)
         {
-            string symbol = "BTC-USD";
-            string symbol2 = "XXBTZUSD";
-            IExchangeAPI apiGDAX = new ExchangeGdaxAPI();
-            IExchangeAPI apiGemini = new ExchangeGeminiAPI();
-            IExchangeAPI apiKraken = new ExchangeKrakenAPI();
-            IExchangeAPI apiBitfinex = new ExchangeBitfinexAPI();
+            string symbol = "XRP";
+
+            List<IExchangeAPI> apis = new List<IExchangeAPI>();
+            apis.Add(new ExchangeGdaxAPI());
+            apis.Add(new ExchangeGeminiAPI());
+            apis.Add(new ExchangeKrakenAPI());
+            apis.Add(new ExchangeBitfinexAPI());
+            apis.Add(new ExchangeBithumbAPI());
 
             while (true)
             {
-                ExchangeTicker ticker = apiGDAX.GetTicker(symbol);
-                ExchangeOrderBook orders = apiGDAX.GetOrderBook(symbol);
-                decimal askAmountSum = orders.Asks.Sum(o => o.Amount);
-                decimal askPriceSum = orders.Asks.Sum(o => o.Price);
-                decimal bidAmountSum = orders.Bids.Sum(o => o.Amount);
-                decimal bidPriceSum = orders.Bids.Sum(o => o.Price);
+                StringBuilder builder = new StringBuilder();
 
-                ExchangeTicker ticker2 = apiGemini.GetTicker(symbol);
-                ExchangeOrderBook orders2 = apiGemini.GetOrderBook(symbol);
-                decimal askAmountSum2 = orders2.Asks.Sum(o => o.Amount);
-                decimal askPriceSum2 = orders2.Asks.Sum(o => o.Price);
-                decimal bidAmountSum2 = orders2.Bids.Sum(o => o.Amount);
-                decimal bidPriceSum2 = orders2.Bids.Sum(o => o.Price);
+                foreach (IExchangeAPI api in apis)
+                {
+                    try
+                    {
+                        ExchangeTicker ticker = api.GetTicker(symbol);
+                        ExchangeOrderBook orders = api.GetOrderBook(symbol);
 
-                ExchangeTicker ticker3 = apiKraken.GetTicker(symbol2);
-                ExchangeOrderBook orders3 = apiKraken.GetOrderBook(symbol2);
-                decimal askAmountSum3 = orders3.Asks.Sum(o => o.Amount);
-                decimal askPriceSum3 = orders3.Asks.Sum(o => o.Price);
-                decimal bidAmountSum3 = orders3.Bids.Sum(o => o.Amount);
-                decimal bidPriceSum3 = orders3.Bids.Sum(o => o.Price);
+                        decimal askAmountSum = orders.Asks.Sum(o => o.Amount);
+                        decimal askPriceSum = orders.Asks.Sum(o => o.Price);
+                        decimal bidAmountSum = orders.Bids.Sum(o => o.Amount);
+                        decimal bidPriceSum = orders.Bids.Sum(o => o.Price);
+                        
+                        string line = String.Format("{0}\n\t LastPrice: {1:0.00}\n\t PriceAmount: {2:0.00}\n\t AskAmountSum: {3:0.00}\n\t AskPriceSum: {4:0.00}\n\t BidAmountSum: {5:0.00}\n\t BidPriceSum: {6:0.00}", api.Name, ticker.Last, ticker.Volume.PriceAmount, askAmountSum, askPriceSum, bidAmountSum, bidPriceSum);
 
-                ExchangeTicker ticker4 = apiBitfinex.GetTicker(symbol);
-                ExchangeOrderBook orders4 = apiBitfinex.GetOrderBook(symbol);
-                decimal askAmountSum4 = orders4.Asks.Sum(o => o.Amount);
-                decimal askPriceSum4 = orders4.Asks.Sum(o => o.Price);
-                decimal bidAmountSum4 = orders4.Bids.Sum(o => o.Amount);
-                decimal bidPriceSum4 = orders4.Bids.Sum(o => o.Price);
+                        builder.AppendLine(line);
+
+                    }catch(Exception e)
+                    {
+                    }
+                }
 
                 Console.Clear();
-                Console.WriteLine("GDAX: {0:0.00}, {1:0.00}, {2:0.00}, {3:0.00}, {4:0.00}, {5:0.00}", ticker.Last, ticker.Volume.PriceAmount, askAmountSum, askPriceSum, bidAmountSum, bidPriceSum);
-                Console.WriteLine("GEMI: {0:0.00}, {1:0.00}, {2:0.00}, {3:0.00}, {4:0.00}, {5:0.00}", ticker2.Last, ticker2.Volume.PriceAmount, askAmountSum2, askPriceSum2, bidAmountSum2, bidPriceSum2);
-                Console.WriteLine("KRAK: {0:0.00}, {1:0.00}, {2:0.00}, {3:0.00}, {4:0.00}, {5:0.00}", ticker3.Last, ticker3.Volume.PriceAmount, askAmountSum3, askPriceSum3, bidAmountSum3, bidPriceSum3);
-                Console.WriteLine("BITF: {0:0.00}, {1:0.00}, {2:0.00}, {3:0.00}, {4:0.00}, {5:0.00}", ticker4.Last, ticker4.Volume.PriceAmount, askAmountSum4, askPriceSum4, bidAmountSum4, bidPriceSum4);
+                Console.Write(builder.ToString());
+
                 Thread.Sleep(5000);
             }
         }
